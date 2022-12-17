2d27fe46bc699dca559c43ef3902c18549ff3d02
raymond ong <raymondong.nus@gmail.com>
2022-12-17 10:06:43 +0800

    bitwise manipulation for the bitfield

---
+++ messages/bitfield.go
@@ -0,0 +1,27 @@
+package messages
+
+// A bitfield represents the pieces that a peer has
+type Bitfield []byte
+
+// tells if a bitfield has a particular index set
+func (bf Bitfield) HasPiece(index int) bool {
+	byteIndex := index / 8
+	offset := index % 8
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return false
+	}
+
+	return bf[byteIndex]>>(7-offset)&1 != 0
+}
+
+func (bf Bitfield) SetPiece(index int) {
+	byteIndex := index / 8
+	offset := index % 8
+
+	// silently discard invalid bounded index
+	if byteIndex < 0 || byteIndex >= len(bf) {
+		return
+	}
+
+	bf[byteIndex] |= 7 << (7 - offset)
+}
